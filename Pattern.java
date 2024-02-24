1566cdae967182590d01ef7da3a5d949ec665b46
Aditya Bakshi <153073510+Aditya90456@users.noreply.github.com>
2024-02-24 21:39:02 +0530

    Happy Birthday

--- Pattern.java
+++ Pattern.java
@@ -1,5 +1,5 @@
 public class Pattern {
-    public static void Mahima_Hans() {
+    public static void Mahima_Madam() {
         /*
          * p p
          * p p
@@ -183,9 +183,370 @@ public class Pattern {
            }
            System.out.println();
         }
+        System.out.println();
+        for (int r = 1; r <= n; r++) {
+            for (int line = 1; line <= n; line++) {
+                if (line == 1) {
+
+                    System.out.print("p ");
+                }
+
+            }
+            for (int p = 1; p <= r; p++) {
+                if (p == 1) {
+                    System.out.print(" ");
+                }
+                if (p >= 2) {
+                    System.out.print(" ");
+                }
+            }
+            for (int st = 1; st <= n; st++) {
+                if (st == 1) {
+                    System.out.print("p");
+                }
+
+            }
+            for (int i = 1; i <= n - r; i++) {
+                if (r <= n) {
+                    System.out.print("  ");
+                }
+                if (i == n) {
+                    System.out.print("");
+                }
+
+            }
+            for (int k = 1; k <= n; k += 1) {
+                if (k == 1) {
+                    System.out.print("p");
+                }
+            }
+            for (int sf = 1; sf <= r; sf++) {
+                if (sf >= 1) {
+
+                    System.out.print(" ");
+                }
+            }
+            for (int k = 1; k <= n; k += 1) {
+                if (k == 1) {
+                    System.out.print("p");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
+        for (int m = 1; m <= n; m++) { 
+            for (int h = 1; h <= n; h++) {
+                if (m == 1 || h == 1 || h == n || m==3) {
+                    
+                    System.out.print(" p ");
+                } else {
+                        
+                    System.out.print("   ");
+                }
+           }
+           System.out.println();
+        }
+        System.out.println();
+
+        for (int m = 1; m <= n; m++) { 
+            for (int h = 1; h <= n; h++) {
+                if (m == 1 || h == 1 || h == n || m==3) {
+                    
+                    System.out.print(" p ");
+                } else {
+                        
+                    System.out.print("   ");
+                }
+           }
+           System.out.println();
+        }
+
+        System.out.println();
+        for (int r = 1; r <= n; r++) {
+            for (int line = 1; line <= n; line++) {
+                if (line == 1) {
+
+                    System.out.print("p ");
+                }
+
+            }
+            for (int p = 1; p <= r; p++) {
+                if (p == 1) {
+                    System.out.print(" ");
+                }
+                if (p >= 2) {
+                    System.out.print(" ");
+                }
+            }
+            for (int st = 1; st <= n; st++) {
+                if (st == 1) {
+                    System.out.print("p");
+                }
+
+            }
+            for (int i = 1; i <= n - r; i++) {
+                if (r <= n) {
+                    System.out.print("  ");
+                }
+                if (i == n) {
+                    System.out.print("");
+                }
+
+            }
+            for (int k = 1; k <= n; k += 1) {
+                if (k == 1) {
+                    System.out.print("p");
+                }
+            }
+            for (int sf = 1; sf <= r; sf++) {
+                if (sf >= 1) {
+
+                    System.out.print(" ");
+                }
+            }
+            for (int k = 1; k <= n; k += 1) {
+                if (k == 1) {
+                    System.out.print("p");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
+            for(int i=1;i<=n;i++){
+                for(int j=1;j<=n;j++){
+                    if(i==3 || j==1 ||j==n){
+                        System.out.print(" p ");
+                    }else{
+                       
+                        System.out.print("   "); 
+                    }
+                }
+ 
+            
+            System.out.println();
+        }
+        System.out.println();
+        for(int k=1;k<=n;k++){
+            for(int h=1;h<=n;h++){
+                if (h==n || k==3 || h==1 || k==1) {
+                    System.out.print(" p ");
+                } else {
+                    System.out.print("   ");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
+    for(int l=1;l<=n;l++){  
+        for(int u=1;u<=n;u++){
+            if(u==1 || u==n || l==n || l==1){
+                
+                System.out.print(" p ");
+            }else{
+                
+                System.out.print("   ");
+            }
+        }
+        System.out.println();
+    }     
+    for(int u=1;u<=n;u++){
+        System.out.println(" p");
+        System.out.println();
+    }
+    System.out.println();
+    for(int l=1;l<=n;l++){  
+        for(int u=1;u<=n;u++){
+            if(u==1 || u==n || l==n || l==1){
+                
+                System.out.print(" p ");
+            }else{
+                
+                System.out.print("   ");
+            }
+        }
+        System.out.println();
+    }     
+    for(int u=1;u<=n;u++){
+        System.out.println(" p");
+        System.out.println();
+    }
+    System.out.println();
+    for(int d=1;d<=n;d++){
+        for(int t=1;t<=d;t++){
+            if (t>=d) {
+                System.out.print("p");       
+            } else {
+                
+                System.out.print(" "); 
+            }
+        }
+        for(int m=1;m<=n-d;m++){
+System.out.print("  ");
+        }
+        for(int k=1;k<=d;k++){
+if (k==1) {
+    
+    System.out.print(" p");       
+}
+        }
+        System.out.println();
+    }
+    for(int m=1;m<=n;m++){
+        System.out.print("     p ");
+        System.out.println();
+                }
+                System.out.println();
+                for(int u=1;u<=n;u++){
+                    for(int m=1;m<=n;m++){
+                        if (u==1 || u==n || m==1 || u==3 || m==n) {
+                            
+                        System.out.print("  p");
+                        } else {
+                            
+                        System.out.print("   ");
+                        }
+                    
+                                }
+                                System.out.println();
+                }
+                System.out.println();
+                for(int i=1;i<=n;i++){
+                    for(int j=1 ;j<=n;j++){
+                        if (i==1 || i==n || j==4) {
+                            
+                        System.out.print(" p ");
+                        } else {
+                            
+                        System.out.print("  ");
+                        }
+                    }
+                    System.out.println();
+                }
+                System.out.println();
+                
+                for(int i=1;i<=n;i++){
+                    for(int j=1;j<=n;j++){
+                        if (i==1 || i==n || j==1 || j==n) {
+                            
+                        System.out.print(" p ");
+                        } else {
+                            
+                        System.out.print("   ");
+                        }
+                    }
+
+                             
+                
+                    System.out.println();
+                }                
+                
+                for(int m=1;m<=n;m++){  
+                            
+                    for(int i=1;i<=m;i++){
+                        if (i>=m || i==1) {
+                            
+                            System.out.print(" p ");
+                            } else {
+                                
+                            System.out.print("   ");
+                            }
+                    
+                            
+                        }
+                        
+                    System.out.println();
+                }
+                System.out.println();
+                for(int i=1;i<=n;i++){
+                    for(int j=1;j<=n;j++){
+                        
+                        if (i==1 ||i==n||j==1 ||j==n) {
+                            
+                            System.out.print(" p ");
+                            } else {
+                                
+                            System.out.print("   ");
+                            }
+                    }
+                    
+                    System.out.println();
+                }
+                for(int i=1;i<=n;i++){
+                    System.out.println("      p");
+                }
+                
+                for(int i=1;i<=n;i++){
+                    for(int j=1;j<=n;j++){
+                        
+                        if (j==1 || j==n || i==3) {
+                            
+                            System.out.print(" p ");
+                            } else {
+                                
+                            System.out.print("   ");
+                            }
+                    }
+                    
+                    System.out.println();
+                }
+                System.out.println();               
+                for(int i=1;i<=n;i++){
+                    for(int j=1;j<=n;j++){
+                        
+                        if (i==1 ||i==n||j==1 ||j==n) {
+                            
+                            System.out.print(" p ");
+                            } else {
+                                
+                            System.out.print("   ");
+                            }
+                    }
+                    
+                    System.out.println();
+                } System.out.println();               
+                for(int i=1;i<=n;i++){
+                    for(int j=1;j<=n;j++){
+                        
+                        if (i==1 ||i==3||j==1 ||j==n) {
+                            
+                            System.out.print(" p ");
+                            } else {
+                                
+                            System.out.print("   ");
+                            }
+                    }
+                    
+                    System.out.println();
+                }//////
+                System.out.println();
+    for(int d=1;d<=n;d++){
+        for(int t=1;t<=d;t++){
+            if (t>=d) {
+                System.out.print("p");       
+            } else {
+                
+                System.out.print(" "); 
+            }
+        }
+        for(int m=1;m<=n-d;m++){
+System.out.print("  ");
+        }
+        for(int k=1;k<=d;k++){
+if (k==1) {
+    
+    System.out.print(" p");       
+}
+        }
+        System.out.println();
+    }
+    for(int m=1;m<=n;m++){
+        System.out.print("     p ");
+        System.out.println();
+                }
+      
 }
 
     public static void main(String[] args) {
-        Mahima_Hans();
+        Mahima_Madam();
     }
 }
